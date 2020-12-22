11f0804b5ed29659af6cf16136d67bbee0209c45
xavierloos <xavier.loos04@gmail.com>
2020-12-22 16:17:25 +0000

    Helpers Methods

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,12 +1,13 @@
 require "card"
 RSpec.describe Card do
-  before do
-    @card = Card.new("Ace", "Spades")
+  def card
+    Card.new("Ace", "Spades")
   end
+
   it "has a rank" do
-    expect(@card.rank).to eq "Ace"
+    expect(card.rank).to eq "Ace"
   end
   it "has a suit" do
-    expect(@card.suit).to eq "Spades"
+    expect(card.suit).to eq "Spades"
   end
 end
