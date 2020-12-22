fdd7e8275478d7a6d89f1594914b68e62b224be3
xavierloos <xavier.loos04@gmail.com>
2020-12-22 16:51:59 +0000

    Reducing Duplication: The LET Method

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,14 +1,13 @@
 require "card"
 RSpec.describe Card do
-  def card
-    Card.new("Ace", "Spades")
-  end
+  let(:card) { Card.new("Ace", "Spades") }
 
-  it "has a rank" do
+  it "has a rank and can change" do
     expect(card.rank).to eq "Ace"
     card.rank = "Queen"
     expect(card.rank).to eq "Queen"
   end
+
   it "has a suit" do
     expect(card.suit).to eq "Spades"
   end
