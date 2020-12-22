2bd93664dc2f476fcc803d267f1cb40e8554a7bf
xavierloos <xavier.loos04@gmail.com>
2020-12-22 16:26:44 +0000

    Problems With Mutation

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Card do
 
   it "has a rank" do
     expect(card.rank).to eq "Ace"
+    card.rank = "Queen"
+    expect(card.rank).to eq "Queen"
   end
   it "has a suit" do
     expect(card.suit).to eq "Spades"
