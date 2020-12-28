94a041e57ec192fb655a267f562b84249c152d07
xavierloos <xavier.loos04@gmail.com>
2020-12-28 22:28:55 +0000

    Replacing An Object With A Double

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -4,7 +4,7 @@ class Actor
   end
 
   def ready?
-    sleep(3)
+    sleep(3) # this slow down out test so in this point this is a good example for mocking
     true
   end
 
@@ -39,6 +39,25 @@ end
 
 # The movie class doesn't really care about how the Actor is going to do that
 # The only real thing Movie cares about is that these method are available to an Actor so they can communicate them to the actor object and tell it what to perform
-actor = Actor.new("Brad Pitt")
-movie = Movie.new(actor)
-movie.start_shooting
+# actor = Actor.new("Brad Pitt")
+# movie = Movie.new(actor)
+# movie.start_shooting
+
+RSpec.describe Movie do
+  # We want to use a mock to test
+  # We call the same objects as the real Actor class
+  let(:stuntman) { double("Mr. Danger", ready?: true, act: "Quote", fall_off_ladder: true, light_on_fire: true) }
+  # this expects an Actor object but we will use our stuntman becaus probably we haven't built the Actor class yet and stuntman responds to the 4 methods called in start_shooting
+  subject { described_class.new(stuntman) }
+
+  describe "#start_shooting method" do
+    it " expects the actor to do 3 actions" do
+      # we write our expetations before we invoke start_shooting method, this is called a behavior driven style
+      expect(stuntman).to receive(:ready?)
+      expect(stuntman).to receive(:act)
+      expect(stuntman).to receive(:fall_off_ladder)
+      expect(stuntman).to receive(:light_on_fire)
+      subject.start_shooting
+    end
+  end
+end
